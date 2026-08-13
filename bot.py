379630fd2ce6ba84c0eb2715f1e9e44dc2c758a7
yt953 <u85254559@gmail.com>
2026-08-13 09:58:15 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "😂 Salom! KulgiBox'ga xush kelibsiz! 🔥"
+    )
+
+bot.infinity_polling()
